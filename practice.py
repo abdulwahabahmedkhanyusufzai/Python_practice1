4d13644bf3719fd9b72f59f4e8807b238d6aafad
Abdul Wahab Ahmed Khan <absurw@gmail.com>
2026-01-18 08:52:04 +0500

    Add initial Python practice script demonstrating basic data types and printing "Hello World".

---
+++ practice.py
@@ -0,0 +1,20 @@
+#This is my first python program
+print("Hello World")
+
+#variable
+#str
+name = "Abdul Wahab Ahmed Khan"
+#int
+age = 21
+#float
+height = 5.9
+#bool
+is_active = True
+#complex
+complex_num = 3 + 4j
+
+print(f"Hello {name} {type(name)}")
+print(f"Age: {age} {type(age)}")
+print(f"Height: {height} {type(height)}")
+print(f"Is Active: {is_active} {type(is_active)}")
+print(f"Complex Number: {complex_num} {type(complex_num)}")
